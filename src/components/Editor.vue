69cdd0fb55ab72a84a47a354f2c358a01617b18f
JasonOffutt <jason.offutt@gmail.com>
2018-01-18 22:46:02 -0700

    Initial  commit
    First pass at wrapping quill editor in vue w/ custom command/toolbar.

---
+++ src/components/Editor.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="editor"
+    :class="{ focus: hasFocus }"
+    :id="id"
+  >
+    <div ref="editorContainer" />
+  </div>
+</template>
+
+<script>
+import Quill from 'quill';
+import 'quill/dist/quill.core.css';
+
+export default {
+  name: 'Editor',
+
+  data() {
+    return {
+      editor: null,
+      hasFocus: false,
+    };
+  },
+
+  props: {
+    id: String,
+  },
+
+  mounted() {
+    this.initEditor();
+  },
+
+  methods: {
+    initEditor() {
+      this.editor = new Quill(this.$refs.editorContainer);
+
+      this.editor.on('selection-change', () => this.handleSelection());
+    },
+
+    handleSelection() {
+      this.hasFocus = this.editor.hasFocus();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .editor {
+    height: 150px;
+    border: 1px solid #dfe0e3;
+    border-radius: 4px;
+
+    &.focus {
+      border: transparent;
+      box-shadow: 0 0 0 0.125rem #58a1d8;
+    }
+  }
+
+  .ql-toolbar {
+    display: none;
+  }
+</style>
